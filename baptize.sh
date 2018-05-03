4955e56bfc6d471366a728a2bd7e10c98fd98c7b
Markus Skyttner <markus.skyttner@nrm.se>
2018-05-03 08:42:38 +0200

    fix issue #25

--- baptize.sh
+++ baptize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-f=$(grep -r -e "https://bioatlas.se" | cut -d: -f1 | sort -d | uniq |grep -e "baptize.sh" -v | xargs)
+f=$(grep -r -e "https://bioatlas.se" * | cut -d: -f1 | sort -d | uniq |grep -e "baptize.sh" -v | xargs)
 
 for i in $f; do
 	echo "sed -i 's,https://bioatlas.se,http://bioatlas.se,g' $i"
