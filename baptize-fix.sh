df82fffb2a4b37ef52a4908605bcdb1365392af1
pragermh <maria.prager@scilifelab.se>
2019-08-21 14:50:57 +0200

    Test

--- baptize-fix.sh
+++ baptize-fix.sh
@@ -5,10 +5,10 @@ else
     sed=sed
 fi
 
-FROM=https://beta.bioatlas.se
-TO=http://beta.bioatlas.se
+FROM=http://auth.bioatlas.se
+TO=https://auth.bioatlas.se
 
-f=$(grep -r -e $FROM * | cut -d: -f1 | sort -d | uniq |grep -e "baptize-fix.sh" -v | xargs)
+f=$(grep -r -e $FROM * | cut -d: -f1 | sort -d | uniq |grep -e "baptize-fix-2.sh" -v | xargs)
 
 for i in $f; do
     echo "sed -i 's,$FROM,$TO,g' $i"
