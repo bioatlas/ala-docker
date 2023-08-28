02154548deed2f35d51536772f98adf43a74094c
shahmanash <manash.shah@gmail.com>
2023-08-28 12:26:04 +0000

    add volumes, updated image version, mount point for /tmp added

--- runBiocache.sh
+++ runBiocache.sh
@@ -1,2 +1,27 @@
-#!/bin/bash
-docker run --rm --network=basnet -v beta_data_nameindex:/data/lucene/namematching -v beta_data_biocachebackend:/data --mount type=bind,source="$(pwd)"/config/blacklistMediaUrls.txt,target=/data/biocache/config/blacklistMediaUrls.txt --mount type=bind,source="$(pwd)"/config/biocache-config.properties,target=/data/biocache/config/biocache-config.properties  -e BIOCACHE_MEMORY_OPTS="-Xmx16g -Xms1g" -it bioatlas/ala-biocachebackend:v0.8 ash
+#!/bin/sh
+
+echo "Creating volume for nameindex using global nameindex data"
+
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=/docker_nfs/var/volumes/data_nameindex \
+    --opt o=bind ala_data_nameindex
+
+echo "Creating volume for biocachebackend"
+
+docker volume create --driver local \
+    --opt type=none \
+    --opt device=/docker_nfs/var/volumes/data_biocachebackend \
+    --opt o=bind ala_data_biocachebackend
+
+echo "Starting biocachebackend container"
+
+docker run --rm --network=basnet \
+-v ala_data_nameindex:/data/lucene/namematching \
+-v ala_data_biocachebackend:/data \
+--mount type=bind,source="$(pwd)"/config/blacklistMediaUrls.txt,target=/data/biocache/config/blacklistMediaUrls.txt \
+--mount type=bind,source="$(pwd)"/config/biocache-config.properties,target=/data/biocache/config/biocache-config.properties  \
+--mount type=bind,source=/data/temp,target=/tmp \
+-e BIOCACHE_MEMORY_OPTS="-Xmx16g -Xms1g" \
+-it bioatlas/ala-biocachebackend:v2.6.1 bash
+
